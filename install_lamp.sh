b705e85f349f34cf8978102bf11f993018a213c8
dormnacy <dormancy.jt@icloud.com>
2015-06-28 01:12:06 +0800

    添加 lamp 安装脚本

---
+++ install_lamp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# php 
+# --with-mysql --with-mysqli --with-mcrypt --with-zlib  --with-freetype-dir --with-jpeg-dir --with-png-dir --with-libxml-dir --with-gd --with-bz2 --with-mhash --with-pcre-regex --enable-pdo --enable-inline-optimization --enable-fpm --enable-mbstring --enable-gd-native-ttf --enable-gd-jis-conv --enable-sockets --enable-sysvsem --enable-sysvshm --enable-pcntl --enable-mbregex --enable-zip  --with-pdo-mysql --enable-intl   --enable-soap --with-xmlrpc --with-curl  --with-xsl --with-readline --with-openssl --enable-intl
+# config libmcrypt
+yum install libxml2 libxml2-devel bzip2 bzip2-devel curl libcur-devel libjpeg-turbo libjpeg-turbo-devel libpng libpng-devel  libXpm libXpm-devel  freetype freetype-devel icu libicu-devel readline readline-devel openssl openssl-devel
