280f3dec67e43760ecb9c84bebf7f0673b13b827
dormnacy <dormancy.jt@icloud.com>
2015-06-27 23:55:40 +0800

    add install mysqld

--- CentOS.sh
+++ CentOS.sh
@@ -3,3 +3,7 @@
 yum install vim git
 #install build-essential
 yum groupinstall 'Development Tools' 
+# install mysql on centos 7
+wget http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm
+rpm -ivh mysql-community-release-el7-5.noarch.rpm
+yum install mysql-community-server
